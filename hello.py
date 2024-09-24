dd8c120f7c968ee113f2a0b78ace3335f964a81c
Siddhant Topiwala <sidd.topiwala02@gmail.com>
2024-09-24 17:57:53 -0400

    import bootstrap and moment to set up main page with added nav bar and date

--- hello.py
+++ hello.py
@@ -1,10 +1,17 @@
-from flask import Flask 
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime, timezone
+
 app = Flask( __name__ ) 
 
 @app.route('/') 
 def index(): 
-    return '<h1>Hello World</h1>'
+    return render_template('index.html', current_time=datetime.now(timezone.utc))
 
 @app.route('/user/<name>') 
 def user(name): 
-    return '<h1>Hello, {}!</h1>'.format(name)
+    return render_template('user.html', name=name)
+
+bootstrap = Bootstrap(app)
+moment = Moment(app)
